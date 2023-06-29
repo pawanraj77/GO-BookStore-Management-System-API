cdf9bf701e6acebf09148dd4e212e5ffef7fa1c3
Pawan Raj <pawansingh201326@gmail.com>
2023-06-29 11:54:49 +0530

    Add the main files

---
+++ cmd/main/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+	"github.com/pawan/go-bookstore/pkg/routes"
+)
+
+func main() {
+	r := mux.NewRouter()
+	routes.RegisterBookRoutes(r)
+	http.Handle("/", r)
+	log.Fatal(http.ListenAndServe("localhost:1901", r))
+
+}
